4178255aabe1508a36930bc9153a46d81f5db53a
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-06-25 16:42:30 +0500

    Added a page for changing the user profile

--- a_users/urls.py
+++ a_users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from a_users.views import *
 
+
 urlpatterns = [
-    path('',profile_view,name='profile')
+    path('',profile_view,name='profile'),
+    path('edit/',profile_edit_view,name='profile-edit')
 ]
+
