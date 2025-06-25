b6dc0a91422e0a03075185300b68ad737b08abca
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-06-25 16:41:24 +0500

    add django_cleanup in installed apps

--- a_core/settings.py
+++ a_core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
     'a_home',
     'a_users',
 ]
@@ -118,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
